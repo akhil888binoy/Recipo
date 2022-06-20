8129dab64a7cffe5339142061706d57c8705c236
akhil888binoy <akhil888binoy@gmail.com>
2022-06-20 21:28:27 +0530

    adding the files

---
+++ my-first-project/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { bufferToggle } from 'rxjs';
+
+@Component({
+  selector: 'app-root', //helps angular to replace the content of index html with app component
+  templateUrl: './app.component.html',
+  //styleUrls: ['./app.component.css']
+  styles: ['h3 { color: red }']
+  
+})
+export class AppComponent {
+  
+}
