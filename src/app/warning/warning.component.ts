fe5f15e4f8962a803acc97b70d5c4a1541477066
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2022-06-20 10:30:14 +0530

    Add files via upload

---
+++ src/app/warning/warning.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: '.app-warning',
+    templateUrl: './warning.component.html'
+    ,styles: ['h4 {color: blue}']
+})
+export class WarningComponent{
+
+}
